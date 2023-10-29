7e15c9e6683614ce386bb8c27cbb5cd3eb8a0dbe
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2023-10-29 20:13:14 -0300

    feat: create animated circle

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import Animated from "react-native-reanimated";
-import { View, StyleSheet } from "react-native";
+import Animated, { useAnimatedRef, useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated";
+import { View, StyleSheet, FlatList } from "react-native";
 import data, { OnboardingData } from "./src/data/data";
 import { RenderItem } from "./src/components/RenderItem";
 
 export default function App() {
+  const flatListRef = useAnimatedRef<FlatList<OnboardingData>>();
+  const x = useSharedValue(0);
 
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: event => {
+      x.value = event.contentOffset.x
+    }
+  })
   return (
     <View
       style={styles.container}
     >
       <Animated.FlatList
-        data={data} 
+        data={data}
+        onScroll={onScroll}
         renderItem={({ item, index }) => {
-          return <RenderItem item={item} index={index} />
+          return <RenderItem item={item} index={index} x={x} />
         }}
         keyExtractor={item => item.id}
         scrollEventThrottle={16}
--- src/components/RenderItem.tsx
+++ src/components/RenderItem.tsx
@@ -2,20 +2,50 @@ import React from "react";
 import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
 import LottieView from "lottie-react-native";
 import { OnboardingData } from "../data/data";
+import Animated, { Extrapolate, SharedValue, interpolate, useAnimatedStyle } from "react-native-reanimated";
+import { transform } from "typescript";
 
 type RenderItemProps = {
   item: OnboardingData;
-  index: number
+  index: number;
+  x: SharedValue<number>;
 }
 
 export function RenderItem({
   index,
   item,
+  x,
 }: RenderItemProps) {
   const {width: SCREEN_WIDTH} = useWindowDimensions();
 
+  const circleAnimation = useAnimatedStyle(() => {
+    const scale = interpolate(
+      x.value,
+      [
+        (index - 1) * SCREEN_WIDTH,
+        index * SCREEN_WIDTH,
+        (index + 1) * SCREEN_WIDTH,
+      ],
+      [1, 4,4],
+      Extrapolate.CLAMP
+    );
+    return {
+      transform: [{scale: scale}]
+    }
+  })
+
   return(
     <View style={[styles.itemContainer, { width: SCREEN_WIDTH }]}>
+      <View style={styles.circleContainer}>
+        <Animated.View style={[
+          { 
+            width: SCREEN_WIDTH, 
+            height: SCREEN_WIDTH, 
+            backgroundColor: item.backgroundColor,
+            borderRadius: SCREEN_WIDTH / 2,
+          }, circleAnimation]} 
+        />
+      </View>
       <View>
         <LottieView 
           source={item.animation} 
@@ -42,5 +72,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
     marginHorizontal: 20
+  },
+  circleContainer: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   }
 })
